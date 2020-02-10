294278de237793dd7e52076a609dc0c998a6da13
tfiosecmtv <tfiosecmtv@gmail.com>
2020-02-11 00:27:21 +0600

    express session

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -37,47 +39,35 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+	name: 'session-id',
+	secret: '12345-67890-09876-54321',
+	saveUninitialized: false,
+	resave: false,
+	store: new FileStore()
+}));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-	console.log(req.signedCookies);
+	console.log(req.session);
 
-	if(!req.signedCookies.user) {
-		var authHeader = req.headers.authorization;
-
-	if(!authHeader) {
+	if(!req.session.user) {
 		var err = new Error('You are not authenticated!');
-
-		res.setHeader('WWW-Authenticate', 'Basic');
 		err.status = 401;
-		next(err);
-	}
-
-	var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-	var username = auth[0];
-	var password = auth[1];
-
-	if (username === 'admin' && password === 'password') {
-		res.cookie('user', 'admin', {signed: true});
-		next();
+		return next(err);
 	}
 	else {
-		var err = new Error('You are not authenticated!');
-
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		next(err);
-	}
-	}
-	else {
-		if(req.signedCookies.user === 'admin') {
+		if(req.session.user === 'authenticated') {
 			next();
 		}
 		else {
 			var err = new Error('You are not authenticated!');
 			err.status = 401;
-			next(err);
+			return next(err);
 		}
 	}
 
@@ -87,10 +77,6 @@ function auth(req, res, next) {
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
