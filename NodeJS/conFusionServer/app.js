a9fe869eb942262a02c465740c65f1bdebda310e
tfiosecmtv <tfiosecmtv@gmail.com>
2020-02-10 22:54:37 +0600

    cookies added

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -37,12 +37,13 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth(req, res, next) {
-	console.log(req.headers);
+	console.log(req.signedCookies);
 
-	var authHeader = req.headers.authorization;
+	if(!req.signedCookies.user) {
+		var authHeader = req.headers.authorization;
 
 	if(!authHeader) {
 		var err = new Error('You are not authenticated!');
@@ -52,12 +53,13 @@ function auth(req, res, next) {
 		next(err);
 	}
 
-	var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+	var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
 
 	var username = auth[0];
 	var password = auth[1];
 
 	if (username === 'admin' && password === 'password') {
+		res.cookie('user', 'admin', {signed: true});
 		next();
 	}
 	else {
@@ -67,6 +69,19 @@ function auth(req, res, next) {
 		err.status = 401;
 		next(err);
 	}
+	}
+	else {
+		if(req.signedCookies.user === 'admin') {
+			next();
+		}
+		else {
+			var err = new Error('You are not authenticated!');
+			err.status = 401;
+			next(err);
+		}
+	}
+
+	
 }
 
 app.use(auth);
